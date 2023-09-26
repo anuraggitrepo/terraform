8d6c98d421910f5528ff5823389200feab62bed5
anuraggitrepo <akishore13@gmail.com>
2023-09-27 02:58:16 +0530

    new changes

--- main.tf
+++ main.tf
@@ -2,51 +2,35 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group
   location = "eastus"
 }
-resource "azurerm_public_ip" "public_ip" {
-  name                = "${random_pet.prefix.id}-public-ip"
-  location            = azurerm_resource_group.rg.location
-  azurerm_resource_group = azurerm_resource_group.rg.id
-  allocation_method   = "Dynamic"
-}
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network
-  location            = azurerm_resource_group.rg.location
-  azurerm_resource_group = azurerm_resource_group.rg.id
-}
-resource "azurerm_subnet" "subnet" {
+data "azurerm_subnet" "subnet" {
   name                 = var.subnet
-  azurerm_resource_group = azurerm_resource_group.rg.id
+  virtual_network_name = var.virtual_network
+  resource_group_name  = var.resource_group
 }
-resource "azurerm_network_interface" "Windows-nic" {
-  name                = "${random_pet.prefix.id}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_public_ip" "publicip" {
+    name                         = "myPublicIP"
+    location                     = "${data.azurerm_resource_group.rg.location}"
+    resource_group_name          = "${data.azurerm_resource_group.rg.name}"
+    public_ip_address_allocation = "dynamic"
+}
+resource "azurerm_network_interface" "nic-card" {
+  name                = "nic-card"
+  location            = "${data.azurerm_resource_group.rg.location}"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
 
-   ip_configuration {
-    name                          = "my_nic_configuration"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "${data.azurerm_subnet.subnet.id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.publicip.id}"
   }
 }
-resource "random_password" "password" {
-  length  = 16
-  special = true
-  lower   = true
-  upper   = true
-  numeric = true
-}
-
-resource "azurerm_windows_virtual_machine" "Virtual_Machine" {
-  count = 11
+count = 11
   name = "CUSPVMSNRPT0${count.index + 1}"
-  azurerm_resource_group = azurerm_resource_group.rg.id
-  azurerm_virtual_network = azurerm_virtual_network.vnet.id
+  azurerm_resource_group = "${azurerm_resource_group.rg.id}"
+  network_interface_ids = ["${azurerm_network_interface.nic-card.id}"]
   vm_hostname = "CUSPVMSNRPT${count.index + 1}"
-  is_windows_image = true
-  azurerm_subnet = azurerm_subnet.subnet.id
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  location = azurerm_resource_group.rg.location
+  location ="${azurerm_network_interface.nic-card.location}"
   size = "D4as_v4"
   admin_username = "adminuser"
   admin_password = random_password.password.result
@@ -68,3 +52,4 @@ resource "azurerm_windows_virtual_machine" "Virtual_Machine" {
     Workload = "SNF"
   }
 }
+
--- output.tf
+++ output.tf
@@ -3,10 +3,10 @@ output "resource_group_name" {
 }
 
 output "public_ip_address" {
-  value = azurerm_windows_virtual_machine[*].main.public_ip_address
+  value = azurerm_windows_virtual_machine.main.public_ip_address
 }
 
 output "admin_password" {
   sensitive = true
-  value     = azurerm_windows_virtual_machine[*].main.admin_password
+  value     = azurerm_windows_virtual_machine.main.admin_password
 }
